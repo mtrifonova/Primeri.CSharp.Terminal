9dfa309eaa42932f47ca4bbcb4e5fc62352453d2
Mariyana Trifonova <mimi_navy@abv.bg>
2015-08-28 23:35:04 +0300

    Pograma Hello Aula

---
+++ Hello_Aula/Hello_Aula/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_Aula
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello Aula!");
+			Console.ReadKey ();
+		}
+	}
+}
