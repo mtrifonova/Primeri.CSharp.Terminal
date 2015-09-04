6e2a0d45c0932c9e1391c738c9daec92da30ed8b
Mariyana Trifonova <mimi_navy@abv.bg>
2015-09-05 00:18:32 +0300

    MODUL_02 Promenlivi ot tip int

---
+++ Modul_02/Modul_02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Modul_02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//definirane
+			int a = 0, b = 0;
+			int sum = 0;
+			//subirane
+			a = 5;
+			b = 10;
+			sum = a + b;
+				
+			//pisane v konzola 
+			Console.Write ( a );
+			Console.Write (" + ");
+			Console.Write ( b );  
+			Console.Write (" = ");
+			Console.WriteLine ( sum );
+		}
+	}
+}
