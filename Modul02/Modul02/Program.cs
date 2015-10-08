a9aa1d0e7a37b009a5865d4bab59057115fbe60b
Mariyana Trifonova <mimi_navy@abv.bg>
2015-10-09 00:09:57 +0300

    Модул02 -  Работа със стрингове

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -9,10 +9,16 @@ namespace MODUL02
 			//Дефиниране на променливи
 			int a = 0, b = 0;
 			int sum = 0;
+			string program_name = " Модул02 - Примери";
+			string program_version = " 1.0";
 			//Събиране на числа
 			a = 10;
 			b = 5;
 			sum = a + b;
+			// Данни за програмата
+			Console.WriteLine (program_name);
+			Console.WriteLine ( " Версия: " + program_version + " \n\n ");
+
 			// Писане в  конзола
 			Console.Write (a);
 			Console.Write (" + ");
