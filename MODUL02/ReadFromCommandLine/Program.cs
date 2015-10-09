3e1fab4b4a193a30d8787f1482855c7056564989
Mariyana Trifonova <mimi_navy@abv.bg>
2015-10-10 00:40:14 +0300

    Модул 02 - Работа с едномерни масиви

---
+++ MODUL02/ReadFromCommandLine/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			//  Дефиниране на променливи
+
+			int a = 5 , b = 0;
+
+			// Въвеждане на входни параметри
+
+			Console.Write(" Моля  въведете a:");
+			b = Convert.ToInt32( Console.ReadLine() );
+
+			// Печат на резултат
+
+			Console.WriteLine("Резултатът от сбора на a + b е: " + (a + b).ToString() + "\n" );
+
+			string test = "1";
+			test += " , 2";
+			test += " , 100";
+			test += " , 4";
+			test += " , 5";
+			Console.WriteLine ("Събиране +=  : " + test + "\n" );
+
+			test = test.Replace (" ,", ";");
+			Console.WriteLine ("Работa с Replace : " + test + "\n" );
+
+			Console.WriteLine ("3-ти знак : " + test.Split(';')[2] + "\n" );
+
+
+
+
+
+
+
+
+
+		}
+
+	}
+}
+				
---
+++ MODUL02/TableProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TableProject
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			string[] row = new string[3];
+		
+			row[0] = "Ред 1";
+			row[1] = "Ред 2";
+		    row[2] = "Ред 3";
+
+			Console.WriteLine (" Масив row: " + row [0] + ", " + row [1] + ", " + row [2] + "\n");
+
+			string[] parse = "1,2,3,4,5,6,7,8,9".Split (','); // Split(,) разделя с , превръща този стринг в масив
+
+
+			Console.WriteLine (" Броят на елементите в масива  : " + "1,2,3,4,5,6,7,8,9\n е:  " + parse.Length);
+
+			string list1 = string.Join(";", parse);
+
+			Console.WriteLine(" Новият стринг е:\n " + list1 + "\n");
+		}
+	}
+}
