532fb5f1262765962e805a214e4a76be9df5367a
Mariyana Trifonova <mimi_navy@abv.bg>
2015-10-11 23:29:07 +0300

    Модул 03 - Урок.3 - Математически операции

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -6,21 +6,34 @@ namespace Modul03
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
 
-			double a = 0, b = 0;
+
+			double a = 0, b = 0, c =0;
 
 			Console.Write (" > a = ");  a = Convert.ToDouble (Console.ReadLine ());
 			Console.Write (" > b = ");  b = Convert.ToDouble (Console.ReadLine ());
 
-		//	Console.WriteLine ("\n\n> a/b : " + (a / b).ToString ("C2"));  // вариант с формат лева 2 знака 
+			 //Console.WriteLine ("\n\n> a/b : " + (a / b).ToString ("C2"));  // вариант с формат лева 2 знака 
 
-		//	Console.WriteLine ("\n\n> a/b : " + (a / b).ToString ("P2"));  // формат процент  2 знака 
-
+		    // Console.WriteLine ("\n\n> a/b : " + (a / b).ToString ("P2"));  // формат процент  2 знака 
+		
+			// форматиране с дименсия
 			string _format = "#0.00 м3";
 
 			Console.WriteLine ("\n\n> a/b : " + (a / b).ToString (_format));  // ръчено добавен  формат 
+		
 
+			// Степенуване 
+			c = Math.Pow (a, b);
+			Console.WriteLine ("\n\n> a ^ b : " + c.ToString (_format)); 
+
+			// коренуване 
+			c = Math.Sqrt (b);
+			Console.WriteLine ("\n\n> b ^1/2 : " + c.ToString (_format)); 
+		
+			// Закръгляне 
+			c = Math.Round(a / b);
+			Console.WriteLine ("\n\n>( a / b ) : " + c.ToString (_format)); 
 		}
 	}
 }
