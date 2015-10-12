33f0eeec9f001daea745c4d4f6dbf2916a302c87
Mariyana Trifonova <mimi_navy@abv.bg>
2015-10-12 23:44:11 +0300

    Модул 03 - Урок.6 - Условен оператор  IF

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -15,21 +15,28 @@ namespace BoolExplore
 
 			int _ui = Convert.ToInt32(Console.ReadLine());  // индекс за това кой елемент от масива искаме
 
-			bool _check01 = _ui <= _i.Length;    // проверка на _ui дали е по-малко от дължината 3 или = 3
-		 
-			bool _check02 = _ui > 0;               // проверка 
+			if (_ui > 0 && _ui <= _i.Length) {
 
-			bool _exp01 = _check01 && _check02;  // Логическо И -&&  трябва и двете да са истина
+				Console.WriteLine (_i [_ui - 1]);
+			} else { if ( _ui > _i.Length ) { Console.WriteLine("Индекса е прекалено голям\n\n"); } 
+				if ( _ui <= 0 ) { Console.WriteLine ("Индекса е прекалено малък\n\n");  }
+			}
 
-			bool _exp02 = _check01 || _check02;  // Логическо Или -|| поне едното да е  истина
-
-			bool _exp03 = !_check01;   // Отрицание
-
-			Console.WriteLine ("Логическо И && :  " + _exp01.ToString());
-
-			Console.WriteLine ("Логическо Или -||  :  " + _exp02.ToString());
-
-			Console.WriteLine (" Отрицание  :  " + _exp03.ToString());
+//			bool _check01 = _ui <= _i.Length;    // проверка на _ui дали е по-малко от дължината 3 или = 3
+//		 
+//			bool _check02 = _ui > 0;               // проверка 
+//
+//			bool _exp01 = _check01 && _check02;  // Логическо И -&&  трябва и двете да са истина
+//
+//			bool _exp02 = _check01 || _check02;  // Логическо Или -|| поне едното да е  истина
+//
+//			bool _exp03 = !_check01;   // Отрицание
+//
+//			Console.WriteLine ("Логическо И && :  " + _exp01.ToString());
+//
+//			Console.WriteLine ("Логическо Или -||  :  " + _exp02.ToString());
+//
+//			Console.WriteLine (" Отрицание  :  " + _exp03.ToString());
 
 			//Console.WriteLine ( _i [_ui - 1 ] );    // пишем -1 понеже C# броенето е от 0 
 
