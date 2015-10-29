45b3fdf0533173139b35ce9b81b4dd7370baaf7e
Mariyana Trifonova <mimi_navy@abv.bg>
2015-10-29 23:53:41 +0200

    Модул3 Променливи от тип Double

---
+++ MODUL3/MODUL3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MODUL3
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Променливи от тип Double (дробни числа) 
+			double a = 0, b = 0;
+
+			Console.Write
+			(" > a = ");     a = Convert.ToDouble (Console.ReadLine ());
+
+			Console.Write (" > b = ");   b = Convert.ToDouble (Console.ReadLine ());
+
+
+			Console.WriteLine ("\n\n a / b  = " + (a / b).ToString ());
+		}
+	}
+}
