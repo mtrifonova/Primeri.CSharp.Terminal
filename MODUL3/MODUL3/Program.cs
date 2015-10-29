609e0ffffcf33b4f3a5cb55b758633421975e9b7
Mariyana Trifonova <mimi_navy@abv.bg>
2015-10-30 00:39:25 +0200

    Модул3 Урок2 -Форматиране Tostring

--- MODUL3/MODUL3/Program.cs
+++ MODUL3/MODUL3/Program.cs
@@ -15,7 +15,10 @@ namespace MODUL3
 			Console.Write (" > b = ");   b = Convert.ToDouble (Console.ReadLine ());
 
 
-			Console.WriteLine ("\n\n a / b  = " + (a / b).ToString ());
+		//	Console.WriteLine ("\n\n a / b  = " + (a / b).ToString ("C2")); CURRENCY цифрата 2 след С =2 знака след запетаята
+		//	Console.WriteLine ("\n\n a / b  = " + (a / b).ToString ("P2")); процент цифрата 2 след Р знака след запетаята
+			string _format = "#0.00 m3";   // хубаво е предварително да се напише типа на формата
+			Console.WriteLine ("\n\n a / b  = " + (a / b).ToString (_format));
 		}
 	}
 }
