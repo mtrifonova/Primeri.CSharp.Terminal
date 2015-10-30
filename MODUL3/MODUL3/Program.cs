f0d59319ce385586b613deb652d4b4b0a21868d3
Mariyana Trifonova <mimi_navy@abv.bg>
2015-10-30 23:21:43 +0200

    Модул3 Урок3 - Математически функции

--- MODUL3/MODUL3/Program.cs
+++ MODUL3/MODUL3/Program.cs
@@ -7,18 +7,24 @@ namespace MODUL3
 		public static void Main (string[] args)
 		{
 			// Променливи от тип Double (дробни числа) 
-			double a = 0, b = 0;
+			double a = 0, b = 0, c = 0;
 
-			Console.Write
-			(" > a = ");     a = Convert.ToDouble (Console.ReadLine ());
+			Console.Write (" > a = ");     a = Convert.ToDouble (Console.ReadLine ());
 
 			Console.Write (" > b = ");   b = Convert.ToDouble (Console.ReadLine ());
 
+		   //	Форматеране с дименсия 
+			  string _format = "#0.00 m3";   // хубаво е предварително да се напише типа на формата
 
-		//	Console.WriteLine ("\n\n a / b  = " + (a / b).ToString ("C2")); CURRENCY цифрата 2 след С =2 знака след запетаята
-		//	Console.WriteLine ("\n\n a / b  = " + (a / b).ToString ("P2")); процент цифрата 2 след Р знака след запетаята
-			string _format = "#0.00 m3";   // хубаво е предварително да се напише типа на формата
-			Console.WriteLine ("\n\n a / b  = " + (a / b).ToString (_format));
+			//	Степенуване
+			c = Math.Pow( a,b );
+			Console.WriteLine ("\n\n a ^ b  = " + c.ToString (_format));
+			//	Коренуване
+			c = Math.Sqrt( b );
+			Console.WriteLine ("\n\n b ^ 1/2  = " + c.ToString (_format));
+			//	Степенуване
+			c = Math.Round( a / b );
+			Console.WriteLine ("\n\n (a / b )  = " + c.ToString (_format));
 		}
 	}
 }
