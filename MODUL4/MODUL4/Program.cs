30ae78dbeadebc44600ead185133e5e4487e40ea
Mariyana Trifonova <mimi_navy@abv.bg>
2016-01-07 23:27:09 +0200

    Модул 4. Видео 1 - Цикъл FOR

---
+++ MODUL4/MODUL4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MODUL4
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			for (int i = 0; i <=5; i+=2)
+			{
+				Console.WriteLine(i.ToString());
+			}
+		}
+	}
+}
