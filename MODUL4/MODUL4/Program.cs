232992c5a159a2e5f8d5b0003349cc6b7a6ab8a1
Mariyana Trifonova <mimi_navy@abv.bg>
2016-01-08 00:46:58 +0200

    МОДУЛ 4.Видео 3 - Примери за влагане на цикли

--- MODUL4/MODUL4/Program.cs
+++ MODUL4/MODUL4/Program.cs
@@ -6,16 +6,32 @@ namespace MODUL4
 	{
 		public static void Main (string[] args)
 		{
-			string [] _table = { "Иван", "Гошо", "Гергана" };  // масив от тип стринг от тип с име table 
+			string[ , ] _table = {
+				{ "Иван", "Георгиев", "19" },  
+				{ "Георги ", "Георгиев", "25" },
+				{ "Владимир", "Павлов", "32" },  
+				{ "Теди", "Тодоров", "50" },			
+				{ "Цецо", "Георгиев", "40" }, 
+				{ "Лора", "Трифонова", "16" } 
+			};
 
-			for (int i = 0; i < _table.Length;i++)    // за i = 0 и i <дължината на таблицата 3;i++ - нараства с 1
-			{
-         // антетка 
-				Console.WriteLine("Име");
-				Console.WriteLine("===========");
-		//		тяло на таблицата
-				Console.WriteLine( _table[i]);
+			// антетка 
+			Console.WriteLine("Име".PadRight(12)+ "Фамилия".PadRight(12)+"Години".PadRight(12)) ;
+			Console.WriteLine("==============================");
+
+			for (int i = 0; i < _table.Length /3 ; i++)    // за i = 0 и i /3 = БРОЯ НА РЕДОВЕТЕ които ще обходим
+			      {
+				for (int j	= 0; j < 3; j++)    //  във 2-рия цикъл за всеки ред ще обходим броя на колоните 
+				{
+					//		тяло на таблицата
+					Console.Write( _table[i,j].PadRight(12));	
+				}
+
+				Console.WriteLine ();
+		
 			}
+		
 		}
+
 	}
 }
