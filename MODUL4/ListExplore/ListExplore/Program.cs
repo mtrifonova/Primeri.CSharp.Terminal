fc149d67698527176a6d1f1b169be4786eaa4365
Mariyana Trifonova <mimi_navy@abv.bg>
2016-01-10 00:48:49 +0200

    Модул 4 -  LIST

---
+++ MODUL4/ListExplore/ListExplore/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			string  _userinput = "";
+			List<int> _list = new List<int> ();
+			_list.Add (5);
+			_list.Add (3);
+			_list.Add (9);
+
+			do {
+				Console.Write ("?");
+			    _userinput = Console.ReadLine ();
+
+				//Добавяне на стойности
+
+				// Преглед на List
+				if ( _userinput.Contains("show"))
+					{
+						for ( int i = 0; i < _list.Count; i++ )
+						{
+							Console.Write("_List: ");
+							Console.Write(_list[i]);
+						if ( i != _list.Count - 1)   Console.Write( ",");
+						}
+							Console.WriteLine();
+					}
+
+
+				// размер на list
+
+
+
+
+			   } while (_userinput != "exit");
+
+
+		}
+	}
+}
