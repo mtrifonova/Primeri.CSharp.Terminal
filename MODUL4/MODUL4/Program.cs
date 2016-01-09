475974e3fe06077f184017b5e3211e5bc9188400
Mariyana Trifonova <mimi_navy@abv.bg>
2016-01-09 23:24:31 +0200

    Модул4 - Цикъл  DO WHILE

--- MODUL4/MODUL4/Program.cs
+++ MODUL4/MODUL4/Program.cs
@@ -19,19 +19,51 @@ namespace MODUL4
 			Console.WriteLine("Име".PadRight(12)+ "Фамилия".PadRight(12)+"Години".PadRight(12)) ;
 			Console.WriteLine("==============================");
 
-			for (int i = 0; i < _table.Length /3 ; i++)    // за i = 0 и i /3 = БРОЯ НА РЕДОВЕТЕ които ще обходим
-			      {
-				for (int j	= 0; j < 3; j++)    //  във 2-рия цикъл за всеки ред ще обходим броя на колоните 
-				{
+			//for (int i = 0; i < _table.Length /3 ; i++)    // за i = 0 и i /3 = БРОЯ НА РЕДОВЕТЕ които ще обходим
+			//      {
+		//		for (int j	= 0; j < 3; j++)    //  във 2-рия цикъл за всеки ред ще обходим броя на колоните 
+		//		{
 					//		тяло на таблицата
-					Console.Write( _table[i,j].PadRight(12));	
-				}
+		//			Console.Write( _table[i,j].PadRight(12));	
+		//		}
 
-				Console.WriteLine ();
+		//		Console.WriteLine ();
 		
+//			int i =0 ;
+//			while (i < _table.Length /3) 
+//			
+//			{
+//				Console.WriteLine (
+//					
+//					_table [i, 0].PadRight(12) +
+//					_table [i, 1].PadRight(12) +
+//					_table [i, 2].PadRight(12) 
+//				);
+//
+//
+//				i++;													
+//			}	
+   	
+			// Примери с DO WHILE  
+
+			int i = 0;
+			//Kod
+
+			do {
+
+				Console.WriteLine (
+					_table [i, 0].PadRight (12) +
+					_table [i, 1].PadRight (12) +
+					_table [i, 2].PadRight (12)
+				);  
+				i++;
 			}
-		
-		}
+			while (i < _table.Length / 3) ;
 
+		} 
 	}
 }
+
+
+
+
