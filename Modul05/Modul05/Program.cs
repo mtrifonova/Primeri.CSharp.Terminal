1ff74620f7b380113369ef5cf9fcd089b9918ca1
Mariyana Trifonova <mimi_navy@abv.bg>
2016-01-31 00:06:25 +0200

    Модул 5.Референции в методите

--- Modul05/Modul05/Program.cs
+++ Modul05/Modul05/Program.cs
@@ -16,7 +16,32 @@ namespace Modul05
 				Console.Write ("$ ");	usercomand = Console.ReadLine ();  //Console.Readline -това е метод който връща стринг; Console.Write - това е метод който не връща нищо
 
 				if (usercomand.Contains("cmd01")) cmd01 (usercomand);
+
 				if (usercomand.Contains("cmd02"))  Console.WriteLine(cmd02 (usercomand));
+
+				if (usercomand.Contains("cmd03"))  
+				   {
+					double _a =0;
+					if ( cmd03(usercomand, out _a))
+					  {
+						Console.WriteLine( " a*a = " + _a.ToString() );
+					} else { 
+						Console.WriteLine("Некоректно въведени данни:"); 
+					       }
+				}
+					if (usercomand.Contains("cmd04"))
+					   {
+					    double _t = 5, _c=-2;
+
+						if ( cmd04 ( ref _t ) )
+						{ 
+							Console.WriteLine( _t ); 
+						 }
+						if ( cmd04 ( ref  _c) )
+						{ 
+							Console.WriteLine( 0 );
+						}
+					}			    	
 
 			} while (usercomand != "exit");
 
@@ -51,8 +76,31 @@ namespace Modul05
 			return _temp;
 		}
 
+		public static bool cmd03( string _input, out double _i) // Използваме  out  Примерно командата ще бъде нещо такова cmd3 45 и резултата да е 45*45
+		{
+			try{
+				string _p =_input.Split(' ')[1];
 
-		//Да върнат параметри 
+				double __p =0;
+
+				if (double.TryParse(_p, out __p) )
+					{
+						_i = __p*__p;
+						return true;
+					}
+
+			}catch{
+			}
+			_i = 0;  // ако не можем да парснем въведената команда _i е = 0 и връщаме резултат false
+			return false;
+		}
+
+	 	public static bool cmd04 ( ref double _i)
+		{
+			double _temp =_i;
+		 _i = _i* _i ;
+		return( _temp > 0);
+		}
 	}
 
 }
